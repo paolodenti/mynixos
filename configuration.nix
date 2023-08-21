68f06d0376785d24c1f0c1a84f5951eba3fecc52
Paolo Denti <paolo.denti@gmail.com>
2023-08-21 07:39:03 -0700

    initial import

--- configuration.nix
+++ configuration.nix
@@ -117,6 +117,7 @@
       shellAliases = {
         ll = "ls -lA";
         sshpassword = "ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no";
+        clear-nix-boot-menu = "sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       };
       oh-my-zsh = {
         enable = true;
