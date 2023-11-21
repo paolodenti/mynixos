af112a4cc037138308214cdce038eb14c70b0a34
Paolo Denti <paolo.denti@gmail.com>
2023-11-20 20:40:34 -0800

    hw

--- hosts/macmini3/configuration.nix
+++ hosts/macmini3/configuration.nix
@@ -10,7 +10,7 @@ in
 {
   imports =
     [ # Include the results of the hardware scan.
-      ../../hardware-configuration.nix
+      ../../../hardware-configuration.nix
       <home-manager/nixos>
     ];
 
